0de313a0dd6217ab7819efa41fd84381d5582a48
shlee <completed0728@gmail.com>
2022-06-01 22:11:23 +0900

    variable.scss

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,78 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <v-card class="overflow-hidden">
+      <v-app-bar app color="primary elevation-0" dark hide-on-scroll>
+        <div class="d-flex align-center">
+          <v-img
+            alt="Vuetify Logo"
+            class="shrink mr-2"
+            contain
+            src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+            transition="scale-transition"
+            width="40"
+          />
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+          <v-img
+            alt="Vuetify Name"
+            class="shrink mt-1 hidden-sm-and-down"
+            contain
+            min-width="100"
+            src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+            width="100"
+          />
+        </div>
+        <v-app-bar-title>Title</v-app-bar-title>
+        <v-spacer></v-spacer>
+        <!-- <v-btn icon>
+          <v-icon>mdi-open-in-new</v-icon>
+        </v-btn> -->
+        <v-icon class="px-3">mdi-open-in-new</v-icon>
+        <v-icon class="px-3">mdi-open-in-new</v-icon>
 
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
+        <template v-slot:extension>
+          <v-tabs align-with-title :fixed-tabs="false">
+            <v-tab>Tab 1</v-tab>
+            <v-tab>Tab 2</v-tab>
+            <v-tab>Tab 3</v-tab>
+          </v-tabs>
+        </template>
+      </v-app-bar>
+    </v-card>
     <v-main>
+      <!-- <v-banner outlined single-line sticky
+        ><template>
+          <v-tabs fixed-tabs background-color="white">
+            <v-tab> Option </v-tab>
+            <v-tab> Another Selection </v-tab>
+            <v-tab> Items </v-tab>
+            <v-tab> Another Screen </v-tab>
+          </v-tabs>
+        </template></v-banner
+      > -->
+      <v-container style="height: 1500px"> </v-container>
       <router-view />
     </v-main>
+    <v-footer>
+      <v-bottom-navigation :value="value" color="primary" fixed>
+        <v-btn>
+          <span>Recents</span>
+
+          <v-icon>mdi-history</v-icon>
+        </v-btn>
+
+        <v-btn>
+          <span>Favorites</span>
+
+          <v-icon>mdi-heart</v-icon>
+        </v-btn>
+
+        <v-btn>
+          <span>Nearby</span>
+
+          <v-icon>mdi-map-marker</v-icon>
+        </v-btn>
+      </v-bottom-navigation>
+    </v-footer>
   </v-app>
 </template>
 
@@ -43,8 +80,8 @@
   export default {
     name: 'App',
 
-    data: () => ({
-      //
-    }),
+    data: () => ({ value: 1 }),
   };
 </script>
+
+<style scoped></style>
