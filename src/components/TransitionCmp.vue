4598b161047b8de347b4f44edebeda2cc4a78f52
suhan <completed0728@gmail.com>
2022-06-21 16:24:36 +0900

    공통트랜지션 props로 설정 default slide-fade

--- src/components/TransitionCmp.vue
+++ src/components/TransitionCmp.vue
@@ -1,5 +1,18 @@
 <template>
-  <transition name="slide-fade">
+  <transition :name="name">
     <slot></slot>
   </transition>
 </template>
+
+<script>
+  export default {
+    props: {
+      name: {
+        type: String,
+        default: 'slide-fade',
+      },
+    },
+  };
+</script>
+
+<style></style>
