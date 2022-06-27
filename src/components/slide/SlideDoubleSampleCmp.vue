91d5b7e7de30aae910fc95a7bbbd46c9a7084ad0
suhan <completed0728@gmail.com>
2022-06-27 17:35:41 +0900

    api테스트

--- src/components/slide/SlideDoubleSampleCmp.vue
+++ src/components/slide/SlideDoubleSampleCmp.vue
@@ -1,79 +1,79 @@
 <template>
-  <swiper
-    class="swiper"
-    :options="swiperOption"
-    style="background-color: white"
-  >
-    <swiper-slide class="slide-1">
-      <SlideProductItem />
-    </swiper-slide>
-    <swiper-slide><SlideProductItem /></swiper-slide>
-    <swiper-slide><SlideProductItem /></swiper-slide>
-    <swiper-slide><SlideProductItem /></swiper-slide>
-    <swiper-slide><SlideProductItem /></swiper-slide>
-    <swiper-slide><SlideProductItem /></swiper-slide>
-    <swiper-slide><SlideProductItem /></swiper-slide>
-    <swiper-slide><SlideProductItem /></swiper-slide>
-    <swiper-slide><SlideProductItem /></swiper-slide>
-    <swiper-slide><SlideProductItem /></swiper-slide>
-    <div class="swiper-pagination" slot="pagination"></div>
-  </swiper>
+	<swiper
+		class="swiper"
+		:options="swiperOption"
+		style="background-color: white"
+	>
+		<swiper-slide class="slide-1">
+			<SlideProductItem />
+		</swiper-slide>
+		<swiper-slide><SlideProductItem /></swiper-slide>
+		<swiper-slide><SlideProductItem /></swiper-slide>
+		<swiper-slide><SlideProductItem /></swiper-slide>
+		<swiper-slide><SlideProductItem /></swiper-slide>
+		<swiper-slide><SlideProductItem /></swiper-slide>
+		<swiper-slide><SlideProductItem /></swiper-slide>
+		<swiper-slide><SlideProductItem /></swiper-slide>
+		<swiper-slide><SlideProductItem /></swiper-slide>
+		<swiper-slide><SlideProductItem /></swiper-slide>
+		<div class="swiper-pagination" slot="pagination"></div>
+	</swiper>
 </template>
 
 <script>
-  import { Swiper, SwiperSlide } from 'vue-awesome-swiper';
-  import 'swiper/css/swiper.css';
-  import SlideProductItem from '@/components/product/SlideProductItem.vue';
-  export default {
-    name: 'swiper-example-slides-per-column',
-    title: 'Multi row slides layout',
-    components: {
-      Swiper,
-      SwiperSlide,
-      SlideProductItem,
-    },
-    data() {
-      return {
-        swiperOption: {
-          slidesPerView: 3,
-          slidesPerColumn: 2,
-          spaceBetween: 0,
-          // pagination: {
-          //   el: '.swiper-pagination',
-          //   clickable: true,
-          // },
-        },
-      };
-    },
-  };
+	import { Swiper, SwiperSlide } from 'vue-awesome-swiper';
+	import 'swiper/css/swiper.css';
+	import SlideProductItem from '@/components/product/SlideProductItem.vue';
+	export default {
+		name: 'swiper-example-slides-per-column',
+		title: 'Multi row slides layout',
+		components: {
+			Swiper,
+			SwiperSlide,
+			SlideProductItem,
+		},
+		data() {
+			return {
+				swiperOption: {
+					slidesPerView: 3,
+					slidesPerColumn: 2,
+					spaceBetween: 0,
+					// pagination: {
+					//   el: '.swiper-pagination',
+					//   clickable: true,
+					// },
+				},
+			};
+		},
+	};
 </script>
 
 <style lang="scss" scoped>
-  .swiper {
-    height: 300px;
-    width: 100%;
+	.swiper {
+		height: 300px;
+		width: 100%;
 
-    .swiper-slide {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      text-align: center;
-      font-weight: bold;
-      // font-size: $font-size-huge * 2;
-      font-size: 16px;
-      background-color: white;
-    }
-  }
+		.swiper-slide {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			text-align: center;
+			font-weight: bold;
+			// font-size: $font-size-huge * 2;
+			font-size: 16px;
+			background-color: white;
+		}
+	}
 
-  .swiper {
-    height: 100%;
-    margin-left: auto;
-    margin-right: auto;
-    max-height: 400px;
-    .swiper-slide {
-      height: 50%;
-      min-width: 155px; //이미지 사이즈가 정해저야함
-      max-width: 155px; //이미지 사이즈가 정해저야함
-    }
-  }
+	.swiper {
+		height: 100%;
+		margin-left: auto;
+		margin-right: auto;
+		max-height: 400px;
+		.swiper-slide {
+			height: 50%;
+			min-width: 155px; //이미지 사이즈가 정해저야함
+			max-width: 155px; //이미지 사이즈가 정해저야함
+		}
+	}
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  transpileDependencies: ['vuetify'],
+	devServer: {
+		overlay: false,
+		proxy: {
+			'/api': {
+				target: process.env.VUE_APP_DEV_API,
+				//pathRewrite: { '^/': '' },
+				changeOrigin: true,
+				//secure: false,
+			},
+		},
+	},
+	transpileDependencies: ['vuetify'],
 };
